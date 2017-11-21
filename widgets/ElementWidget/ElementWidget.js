da878249f7f938a10ff2599e7cb3d8b531dd6f3c
Kevin Duglué <kevin.duglue@gmail.com>
2017-11-21 14:21:04 +0100

    /ElementWidget: Auto stash before merge of "feature/ElementWidget" and "origin/feature/ElementWidget"

--- widgets/ElementWidget/ElementWidget.js
+++ widgets/ElementWidget/ElementWidget.js
@@ -45,7 +45,8 @@ class ElementWidget extends TUIOWidget {
      */
   onTouchCreation(tuioTouch) {
     super.onTouchCreation(tuioTouch);
-    if (this.isTouched(tuioTouch.x, tuioTouch.y)) {
+    if (this.isTouched(tuioTouch.x, tuioTouch.y) && !ElementWidget.isAlreadyTouched) {
+      ElementWidget.isAlreadyTouched = true;
       this._lastTouchesValues = {
         ...this._lastTouchesValues,
         [tuioTouch.id]: {
@@ -116,6 +117,17 @@ class ElementWidget extends TUIOWidget {
     }
   }
 
+  /**
+   * Call after a TUIOTouch deletion.
+   *
+   * @method onTouchDeletion
+   * @param {number/string} tuioTouchId - TUIOTouch's id to delete.
+   */
+  onTouchDeletion(tuioTouchId) {
+    super.onTouchDeletion(tuioTouchId);
+    ElementWidget.isAlreadyTouched = false;
+  }
+
     /**
    * Call after a TUIOTag creation.
    *
@@ -125,7 +137,7 @@ class ElementWidget extends TUIOWidget {
   onTagCreation(tuioTag) {
     super.onTagCreation(tuioTag);
     console.log(tuioTag);
-    if (this.isTouched(tuioTag.x, tuioTag.y)) {
+    if (this.isTouched(tuioTag.x, tuioTag.y) && !ElementWidget.isAlreadyTouched) {
       this._lastTagsValues = {
         ...this._lastTagsValues,
         [tuioTag.id]: {
@@ -149,10 +161,10 @@ class ElementWidget extends TUIOWidget {
     console.log(this._lastTagsValues);
     if (typeof (this._lastTagsValues[tuioTag.id]) !== 'undefined') {
 
-      if(tuioTag.id == this.idTagDelete) {
+      if (tuioTag.id === this.idTagDelete) {
         this._domElem.remove();
-      }
-      else if (tuioTag.id == this.idTagMove){
+        this.deleteWidget();
+      } else {
         const lastTagValue = this._lastTagsValues[tuioTag.id];
         const diffX = tuioTag.x - lastTagValue.x;
         const diffY = tuioTag.y - lastTagValue.y;
@@ -207,7 +219,21 @@ class ElementWidget extends TUIOWidget {
       } //  else if
 
     }
+
+  }
+
+  /**
+   * Call after a TUIOTag deletion.
+   *
+   * @method onTagDeletion
+   * @param {number/string} tuioTagId - TUIOTag's id to delete.
+   */
+  onTagDeletion(tuioTagId) {
+    super.onTagDeletion(tuioTagId);
+    ElementWidget.isAlreadyTouched = false;
+    console.log('Tag Deleted');
   }
 }
 
+ElementWidget.isAlreadyTouched = true;
 export default ElementWidget;
--- widgets/ElementWidget/VideoElementWidget/VideoElementWidget.js
+++ widgets/ElementWidget/VideoElementWidget/VideoElementWidget.js
@@ -24,7 +24,7 @@ import ElementWidget from '../ElementWidget';
      * @param {number} width - ImageElementWidget's width.
      * @param {number} height - ImageElementWidget's height.
      */
-    constructor(x, y, width, height, src, tagMove, tagDelete, tagZoom, tagInfo) {
+    constructor(x, y, width, height, src, tagMove, tagDelete, tagZoom, tagInfo, tagPlayPause, tagVolume) {
       super(x, y, width, height, tagMove, tagDelete, tagZoom, tagInfo);
       this._domElem = $('<video>');
       this._domElem.attr('src', src);
@@ -34,7 +34,27 @@ import ElementWidget from '../ElementWidget';
       this._domElem.css('position', 'absolute');
       this._domElem.css('left', `${x}px`);
       this._domElem.css('top', `${y}px`);
+      this.idTagPlayPause = tagPlayPause;
+      this.idTagVolume = tagVolume;
     } // constructor
+
+    /**
+     * Call after a TUIOTag update.
+     *
+     * @method onTagUpdate
+     * @param {TUIOTag} tuioTag - A TUIOTag instance.
+     */
+    onTagUpdate(tuioTag) {
+      super.onTagUpdate(tuioTag);
+      if (typeof (this._lastTagsValues[tuioTag.id]) !== 'undefined') {
+        if (tuioTag.id === this.idTagPlayPause) {
+          this._domElem.play();
+        } else if (tuioTag.id === this.idTagVolume) {
+          this._domElem.prop('volume', 0.5);
+          // $("video").prop("volume", 0.5);
+        }
+      }
+    }
   } // class ImageElementWidget
 
   export default VideoElementWidget;
