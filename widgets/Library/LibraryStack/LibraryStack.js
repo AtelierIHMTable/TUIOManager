4a3e9e95e0249324d7a9ed5c68a77fc2392b5411
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-12-04 13:57:55 +0100

    Adding LibraryStack

--- widgets/Library/LibraryStack/LibraryStack.js
+++ widgets/Library/LibraryStack/LibraryStack.js
@@ -1,27 +1,55 @@
 /**
- * @author Kevin Duglue <kevin.duglue@gmail.com>
- * @author Rémy Kaloustian <remy.kaloustian@gmail.com>
+ * @author Christian Brel <ch.brel@gmail.com>
  */
 
+// Import JQuery
 import $ from 'jquery/dist/jquery.min';
-import TUIOWidget from '../../../core/TUIOWidget';
+
+import TUIOWidget from 'tuiomanager/core/TUIOWidget';
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from 'tuiomanager/core/constants';
+import { radToDeg } from 'tuiomanager/core/helpers';
+
+
+
 
 /**
- * Class for a circular menu.
+ * Main class to manage LibraryStack.
  *
- * @class CircularMenu
+ * Note:
+ * It's dummy implementation juste to give an example
+ * about how to use TUIOManager framework.
+ *
+ * @class LibraryStack
  * @extends TUIOWidget
  */
 class LibraryStack extends TUIOWidget {
-
+  /**
+   * ImageWidget constructor.
+   *
+   * @constructor
+   * @param {number} x - ImageWidget's upperleft coin abscissa.
+   * @param {number} y - ImageWidget's upperleft coin ordinate.
+   * @param {number} width - ImageWidget's width.
+   * @param {number} height - ImageWidget's height.
+   */
   constructor(x, y, width, height) {
     super(x, y, width, height);
-    this._domElem = $('<div>');
-    this._domElem.css('top', `${x}`);
-    this._domElem.css('left', `${y}`);
-    this._domElem.css('width', `${width}`);
-    this._domElem.css('height', `${height}`);
-    this._domElem.css('display', 'inline');
+
+    this._lastTouchesValues = {};
+    this._lastTagsValues = {};
+    this._stackList = [];
+    this._angle = 0;
+
+    this._domElem = $('<div class="library-stack"> </div>');
+    //this._domElem.attr('src', imgSrc);
+    this._domElem.css('width', `${width}px`);
+    this._domElem.css('height', `${height}px`);
+    this._domElem.css('position', 'absolute');
+    this._domElem.css('left', `${x}px`);
+    this._domElem.css('top', `${y}px`);
+    this._domElem.css('background-color', '#FF6633');
+
+    allTheStacks.push(this);
   }
 
   /**
@@ -31,27 +59,6 @@ class LibraryStack extends TUIOWidget {
    */
   get domElem() { return this._domElem; }
 
-  addElementWidget(elementWidget) {
-    const newElem = $('<div>')
-      .css('width', 200)
-      .css('height', 200)
-      .css('background-color', '#000')
-      .css('margin-left', '20')
-      .css('display', 'inline');
-    this._domElem.append(newElem);
-  }
-
-  /**
-  * Check if TUIOWidget is touched.
-  *
-  * @method isTouched
-  * @param {number} x - Point's abscissa to test.
-  * @param {number} y - Point's ordinate to test.
-  */
-  isTouched(x, y) {
-    return (x >= this.x && x <= this.width + this.x && y >= this.y && y <= this.y + this.height) && !this.isHide;
-  }
-
   /**
    * Call after a TUIOTouch creation.
    *
@@ -59,7 +66,16 @@ class LibraryStack extends TUIOWidget {
    * @param {TUIOTouch} tuioTouch - A TUIOTouch instance.
    */
   onTouchCreation(tuioTouch) {
-
+    super.onTouchCreation(tuioTouch);
+    if (this.isTouched(tuioTouch.x, tuioTouch.y)) {
+      this._lastTouchesValues = {
+        ...this._lastTouchesValues,
+        [tuioTouch.id]: {
+          x: tuioTouch.x,
+          y: tuioTouch.y,
+        },
+      };
+    }
   }
 
   /**
@@ -69,7 +85,39 @@ class LibraryStack extends TUIOWidget {
    * @param {TUIOTouch} tuioTouch - A TUIOTouch instance.
    */
   onTouchUpdate(tuioTouch) {
+    if (typeof (this._lastTouchesValues[tuioTouch.id]) !== 'undefined') {
+      const lastTouchValue = this._lastTouchesValues[tuioTouch.id];
+      const diffX = tuioTouch.x - lastTouchValue.x;
+      const diffY = tuioTouch.y - lastTouchValue.y;
 
+      let newX = this.x + diffX;
+      let newY = this.y + diffY;
+
+      if (newX < 0) {
+        newX = 0;
+      }
+
+      if (newX > (WINDOW_WIDTH - this.width)) {
+        newX = WINDOW_WIDTH - this.width;
+      }
+
+      if (newY < 0) {
+        newY = 0;
+      }
+
+      if (newY > (WINDOW_HEIGHT - this.height)) {
+        newY = WINDOW_HEIGHT - this.height;
+      }
+
+      this.moveTo(newX, newY);
+      this._lastTouchesValues = {
+        ...this._lastTouchesValues,
+        [tuioTouch.id]: {
+          x: tuioTouch.x,
+          y: tuioTouch.y,
+        },
+      };
+    }
   }
 
   /**
@@ -79,7 +127,16 @@ class LibraryStack extends TUIOWidget {
    * @param {TUIOTag} tuioTag - A TUIOTag instance.
    */
   onTagCreation(tuioTag) {
-
+    super.onTagCreation(tuioTag);
+    if (this.isTouched(tuioTag.x, tuioTag.y)) {
+      this._lastTagsValues = {
+        ...this._lastTagsValues,
+        [tuioTag.id]: {
+          x: tuioTag.x,
+          y: tuioTag.y,
+        },
+      };
+    }
   }
 
   /**
@@ -89,18 +146,110 @@ class LibraryStack extends TUIOWidget {
    * @param {TUIOTag} tuioTag - A TUIOTag instance.
    */
   onTagUpdate(tuioTag) {
+    if (typeof (this._lastTagsValues[tuioTag.id]) !== 'undefined') {
+      const lastTagValue = this._lastTagsValues[tuioTag.id];
+      const diffX = tuioTag.x - lastTagValue.x;
+      const diffY = tuioTag.y - lastTagValue.y;
 
+      let newX = this.x + diffX;
+      let newY = this.y + diffY;
+
+      if (newX < 0) {
+        newX = 0;
+      }
+
+      if (newX > (WINDOW_WIDTH - this.width)) {
+        newX = WINDOW_WIDTH - this.width;
+      }
+
+      if (newY < 0) {
+        newY = 0;
+      }
+
+      if (newY > (WINDOW_HEIGHT - this.height)) {
+        newY = WINDOW_HEIGHT - this.height;
+      }
+
+      this.moveTo(newX, newY, radToDeg(tuioTag.angle));
+      this._lastTagsValues = {
+        ...this._lastTagsValues,
+        [tuioTag.id]: {
+          x: tuioTag.x,
+          y: tuioTag.y,
+        },
+      };
+    }
   }
 
   /**
-   * Call after a TUIOTag deletion.
+   * Move ImageWidget.
    *
-   * @method onTagDeletion
-   * @param {number/string} tuioTagId - TUIOTag's id to delete.
+   * @method moveTo
+   * @param {string/number} x - New ImageWidget's abscissa.
+   * @param {string/number} y - New ImageWidget's ordinate.
+   * @param {number} angle - New ImageWidget's angle.
    */
-  onTagDeletion(tuioTagId) {
+  moveTo(x, y, angle = null) {
+    this._x = x;
+    this._y = y;
+    this._domElem.css('left', `${x}px`);
+    this._domElem.css('top', `${y}px`);
+    if (angle !== null) {
+      this._domElem.css('transform', `rotate(${angle}deg)`);
+    }
+  }//moveTo()
 
+  addElementWidget(elementWidget) {
+    elementWidget._isInStack = true;
+    elementWidget.canMove(false, false);
+    elementWidget.canZoom(false, false);
+    elementWidget.canRotate(false, false);
+    this._stackList.push(elementWidget);
+
+    console.log(elementWidget.constructor.name);
+    this._domElem.append(elementWidget._domElem);
+    const oldWidth = elementWidget._domElem.width();
+    console.log("oldWidth = " + oldWidth);
+    elementWidget._domElem.css('width', this._domElem.width()-20);
+    console.log("newWidth = " + elementWidget._domElem.width());
+    console.log("oldHeight = "+ elementWidget._domElem.height());
+
+    const newHeight = elementWidget._domElem.width() * elementWidget._domElem.height() / oldWidth;
+    console.log("newHeight = "+ newHeight);
+    elementWidget._domElem.css('height', newHeight);
+
+    if(elementWidget._domElem.height() > this._domElem.height()) {
+      const newWidth = elementWidget._domElem.width() - (elementWidget._domElem.height()- this._domElem.height() + 20);
+      elementWidget._domElem.css('width', newWidth+'px');
+      const newHeight = elementWidget._domElem.height() - (elementWidget._domElem.height()- this._domElem.height() + 20);
+      elementWidget._domElem.css('height', newHeight);
+    }
+    elementWidget._domElem.css('transform', 'rotate('+ this._angle+'deg)');
+    elementWidget._domElem.addClass('stack-element');
+    this._angle += 50;
+  }// addElementWidget()
+
+  isInBounds(element) {
+    if(element.x >= this.x && element.x <= (this.x + this.width) && element.y >= this.y && element.y <= (this.y + this.height)) {
+      return true;
+    }
+    return false;
+  }// isInBounds()
+
+}// class LibraryStack
+
+var maglobal = "baybay";
+
+var allTheStacks = [];
+
+var CheckStacks = function (element) {
+  console.log("In CheckStacks() !!!");
+  for (var i = 0; i < allTheStacks.length; i++) {
+    if( allTheStacks[i].isInBounds(element)) {
+      allTheStacks[i].addElementWidget(element);
+      break;
+    }
   }
-}
+}// CheckStacks()
 
 export default LibraryStack;
