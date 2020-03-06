2906f96280c63cf135114117dcaffb5f1849568b
Lucas OMS <lucas.oms@etu.unice.fr>
2020-03-06 20:51:32 +0100

    InitialRotationWidget pour retourner un élément lors de sa création

---
+++ widgets/behavior/utils/InitialRotationWidget.js
@@ -0,0 +1,24 @@
+/**
+ * @author Lucas Oms <lucas.oms@hotmail.fr>
+ */
+import Behavior from '../Behavior';
+
+/**
+ * @class InitialRotationWidget
+ *
+ * Use to add rotate effect on the widget (only set the rotation for widget creation)
+ */
+class InitialRotationWidget extends Behavior {
+  /**
+   * @param {BaseWidget} widget
+   * @param {number} angle to rotate
+   */
+  constructor(widget, angle) {
+    super(widget);
+    const valueWithoutRotate = this.currentTransform.replace(/rotate\([-+]?[0-9]*\.?[0-9]+deg\)/g, '');
+    this.currentTransform = `${valueWithoutRotate} rotate(${angle}deg)`;
+    this.domElem.css('transform', this.currentTransform);
+  }
+}
+
+export default InitialRotationWidget;
